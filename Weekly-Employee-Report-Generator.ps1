feb313417e50426c3cdbaf458fd06afd88bacbb0
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-11-18 13:51:16 +0100

    Script parametrization

--- Weekly-Employee-Report-Generator.ps1
+++ Weekly-Employee-Report-Generator.ps1
@@ -1,5 +1,6 @@
 $Date = Get-Date
 $DayDecrement = 0
+$Settings = Get-Content -Path "$PSScriptRoot\Settings.cfg" | ConvertFrom-StringData
 
 switch ($Date.DayOfWeek) {
     'Monday' {
@@ -24,15 +25,8 @@ switch ($Date.DayOfWeek) {
         $DayDecrement = -6
     }
 }
-
 $MondayDate = $Date.AddDays($DayDecrement).ToString("dd.MM.yyyy.")
-$TuesdayDate = $Date.AddDays($DayDecrement + 1).ToString("dd.MM.yyyy.")
-$WednesdayDate = $Date.AddDays($DayDecrement + 2).ToString("dd.MM.yyyy.")
-$ThursdayDate = $Date.AddDays($DayDecrement + 3).ToString("dd.MM.yyyy.")
-$FridayDate = $Date.AddDays($DayDecrement + 4).ToString("dd.MM.yyyy.")
-$SaturdayDate = $Date.AddDays($DayDecrement + 5).ToString("dd.MM.yyyy.")
 $SundayDate = $Date.AddDays($DayDecrement + 6).ToString("dd.MM.yyyy.")
-
 $Settings = Get-Content -Path "$PSScriptRoot\Settings.cfg" | ConvertFrom-StringData
 
 $ReportTitle = $Settings.ReportTitle + "$MondayDate - $SundayDate"
@@ -42,28 +36,31 @@ $ReportFullName = "$Documents\$ReportFolderName\$ReportTitle" + "md"
 
 if (-not (Test-Path -Path $ReportFullName)) {
     #Defining names of the days in Serbian language
-    $Monday = "Ponedeljak"
-    $Tuesday = "Utorak"
-    $Wednesday = "Sreda"
-    $Thursday = "Četvratak"
-    $Friday = "Petak"
-    $Saturday = "Subota"
-    $Sunday = "Nedelja"
+    $DaysOfWeek = @(
+        'Ponedeljak'
+        'Utorak'
+        'Sreda'
+        'Četvratak'
+        'Petak'
+        'Subota'
+        'Nedelja'
+    )
 
-    $Employee = "> Zoran Jankov - Administrator sistema i računarskih mreža"
-
-    $Separator = "`n`n------`n`n"
-    $WorkingHours = "`n------`n`n### 07:30-08:30`n`n- `n`n### 08:30-09:30`n`n- `n`n### 09:30-10:30`n`n- `n`n### 10:30-11:30`n`n- `n### 11:30-12:30`n`n- `n`n### 12:30-13:30`n`n- `n`n### 13:30-14:30`n`n- `n`n### 14:30-15:30`n`n- "
+    $Employee = "> " + $Settings.EmployeeName + " - " + $Settings.EmployeeTitle
     
-    $ReportBody = "# $ReportTitle`n$Employee"
-    $ReportBody += "$Separator## $Monday $MondayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Tuesday $TuesdayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Wednesday $WednesdayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Thursday $ThursdayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Friday $FridayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Saturday $SaturdayDate`n$WorkingHours"
-    $ReportBody += "$Separator## $Sunday $SundayDate`n$WorkingHours`n`n------"
-
+    $ReportBody = "# $ReportTitle`n`n$Employee`n`n------`n`n"
+    foreach ($Day in $DaysOfWeek) {
+        $ReportBody += "## $Day " + $Date.AddDays($DayDecrement).ToString("dd.MM.yyyy.")
+        $DayDecrement ++
+        $ReportBody += "`n`n------`n`n"
+        for ($Hour = 0; $Hour -lt $Settings.WorkingTime; $Hour ++) {
+            $Start = ([datetime]::ParseExact($Settings.StartingTime, "HH:mm", $null)).AddHours($Hour).ToString("HH:mm")
+            $End = ([datetime]::ParseExact($Settings.StartingTime, "HH:mm", $null)).AddHours($Hour + 1).ToString("HH:mm")
+            $ReportBody += "### $Start - $End`n`n- `n`n"
+        }
+        $ReportBody += "------`n`n"
+    }
+    
     New-Item -Path $ReportFullName -Force
     Add-Content -Path $ReportFullName -Value $ReportBody
 }
